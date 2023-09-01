1525ede94d5a1695b016cf9d1734affa8316eb24
KadamTeju <tejushinde8897@gmail.com>
2023-09-01 19:37:33 +0530

    First Commit

---
+++ Day3assignment/src/com/bridgelabz/Printelement.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Printelement {
+    public static void main(String[] args) {
+        int array[]={1,2,3,4,5};
+        System.out.println("print given array");
+        for(int i=0;i<array.length;i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
